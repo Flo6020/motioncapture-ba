f727ce820642fc44466a977b8a601fbbb53fef02
Florian Just <DEINE_GITHUB_EMAIL>
2026-03-24 17:46:15 +0100

    rtmpose-t kommentiert und video speichert richtig

--- NR1/rtmpose-t.py
+++ NR1/rtmpose-t.py
@@ -1,16 +1,31 @@
-#Real-Time Multi-Person Pose Estimation --tiny
+# Diese API kapselt die komplette Pipeline bestehend aus:
+# - Personenerkennung (Bounding Box Detection)
+# - 2D-Keypoint-Schätzung
+# in einer einzigen einfach nutzbaren Klasse.
 from mmpose.apis import MMPoseInferencer
 
+
+# Initialisierung des Inferenzobjekts.
+# 'rtmpose-t_8xb256-420e_coco-256x192' bedeutet:
+# rtmpose-t      → Tiny-Version von RTMPose (optimiert für geringe Latenz)
+# 8xb256         → Trainings-Batchstruktur (irrelevant für Inferenz)
+# 420e           → 420 Trainings-Epochen während Modelltraining
+# coco           → Trainingsdatensatz (COCO Keypoint Dataset)
+# 256x192        → Eingabebildgröße des Netzwerks
+#
+# Beim ersten Start lädt MMPose automatisch die passenden Gewichte herunter.
 inferencer = MMPoseInferencer(
     pose2d='rtmpose-t_8xb256-420e_coco-256x192'
 )
 
+# Die Funktion gibt einen Generator zurück, der Frame für Frame Ergebnisse liefert.
 results = inferencer(
     'VNpose.jpg',
-    show=False, #in meiner WSL-/Linux-Umgebung funktioniert das Qt/X11-Fenster-Backend nich richtig deswegen auf False
-    out_dir='output'
+    show=True, #True→ aktiviert ein Fenster mit visualisierten Keypoints und Bounding Boxes
+    out_dir='output' # → speichert zusätzlich
 )
 
+# Bei Videos würde diese Schleife alle Frames nacheinander verarbeiten.
 for _ in results:
     pass
 
---
+++ NR2/displaytest.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+print("OpenCV-Version:", cv2.__version__)
+
+# Ausgabe des aktuell verwendeten GUI-Backends von OpenCV
+# z. B. QT, GTK oder WAYLAND
+# wichtig zur Diagnose von Anzeigeproblemen unter Linux/WSL
+print("UI-Backend:", cv2.currentUIFramework())
+
+# Erzeugung eines schwarzen Testbildes mit der Größe 480x640 Pixel
+# Format: Höhe x Breite x Farbkanäle
+img = np.zeros((480, 640, 3), dtype=np.uint8)
+
+#Text
+cv2.putText(
+    img,                        # Zielbild
+    "OpenCV GUI Test",          # Textinhalt
+    (120, 240),                 # Position (x, y)
+    cv2.FONT_HERSHEY_SIMPLEX,   # Schriftart
+    1,                          # Schriftgröße
+    (255, 255, 255),            # Textfarbe (weiß)
+    2                           # Linienstärke
+)
+
+# Öffnet Fenster
+cv2.imshow("test", img)
+
+# Parameter 0: unendlich lange waren
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
